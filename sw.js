f3af5d24650f07f535858fdbaf2aff2630e29e85
bancogratidaooficial-ops <bancogratidao.oficial@gmail.com>
2026-09-12 23:19:12 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'gratidao-v15-1-plus-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
